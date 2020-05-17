10f29fc4c048c745446137f6c1ce58e958c3e9b8
Hadrien Lepousé <hlepouse@gmail.com>
2020-05-17 21:10:41 +0200

    Working component

---
+++ UI/Components/TwitchBotComponent.cs
@@ -0,0 +1,108 @@
+﻿using LiveSplit.Model;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Windows.Forms;
+using LiveSplit.Model.Input;
+using LiveSplit.Web.Share;
+using System.Threading;
+
+namespace LiveSplit.UI.Components
+{
+    public class CounterComponent : IComponent
+    {
+        public CounterComponent(LiveSplitState state)
+        {
+            Settings = new CounterComponentSettings();
+            this.state = state;
+
+            Start();
+            SendMessage("coucou");
+        }
+
+        public void Start()
+        {
+            if (!Twitch.Instance.IsLoggedIn)
+            {
+                var thread = new Thread(() => Twitch.Instance.VerifyLogin()) { ApartmentState = ApartmentState.STA };
+                thread.Start();
+                thread.Join();
+            }
+
+            if (!Twitch.Instance.ConnectedChats.ContainsKey(Twitch.Instance.ChannelName))
+            {
+                Twitch.Instance.ConnectToChat(Twitch.Instance.ChannelName);
+            }
+
+            Twitch.Instance.Chat.OnMessage += OnMessage;
+        }
+
+        private void SendMessage(string message)
+        {
+            Twitch.Instance.Chat.SendMessage("/me " + message);
+        }
+
+        private void OnMessage(object sender, TwitchChat.Message message)
+        {
+            SendMessage("ok");
+        }
+
+        public CounterComponentSettings Settings { get; set; }
+
+        public GraphicsCache Cache { get; set; }
+
+        public float VerticalHeight { get; set; }
+
+        public float MinimumHeight { get; set; }
+
+        public float MinimumWidth { get;}
+
+        public float HorizontalWidth { get; set; }
+
+        public IDictionary<string, Action> ContextMenuControls
+        {
+            get { return null; }
+        }
+
+        public float PaddingTop { get; set; }
+        public float PaddingLeft { get; }
+        public float PaddingBottom { get; set; }
+        public float PaddingRight { get; }
+
+        protected SimpleLabel CounterNameLabel = new SimpleLabel();
+        protected SimpleLabel CounterValueLabel = new SimpleLabel();
+
+        protected Font CounterFont { get; set; }
+
+        private LiveSplitState state;
+
+        public void DrawHorizontal(Graphics g, Model.LiveSplitState state, float height, Region clipRegion) {}
+
+        public void DrawVertical(System.Drawing.Graphics g, Model.LiveSplitState state, float width, Region clipRegion) {}
+
+        public string ComponentName
+        {
+            get { return "TwitchBot"; }
+        }
+
+        public Control GetSettingsControl(LayoutMode mode)
+        {
+            return Settings;
+        }
+
+        public System.Xml.XmlNode GetSettings(System.Xml.XmlDocument document)
+        {
+            return Settings.GetSettings(document);
+        }
+
+        public void SetSettings(System.Xml.XmlNode settings)
+        {
+            Settings.SetSettings(settings);
+        }
+
+        public void Update(IInvalidator invalidator, Model.LiveSplitState state, float width, float height, LayoutMode mode) {}
+
+        public void Dispose() {}
+    }
+}
---
+++ UI/Components/TwitchBotComponentFactory.cs
@@ -0,0 +1,24 @@
+﻿using LiveSplit.Model;
+using System;
+
+namespace LiveSplit.UI.Components
+{
+    public class TwitchBotComponentFactory : IComponentFactory
+    {
+        public string ComponentName => "TwitchBot";
+
+        public string Description => "A Twitch bot inside LiveSplit";
+
+        public ComponentCategory Category => ComponentCategory.Other;
+
+        public IComponent Create(LiveSplitState state) => new CounterComponent(state);
+
+        public string UpdateName => ComponentName;
+
+        public string XMLURL => "http://livesplit.org/update/Components/update.LiveSplit.TwitchBot.xml";
+
+        public string UpdateURL => "http://livesplit.org/update/";
+
+        public Version Version => Version.Parse("1.8.0");
+    }
+}
